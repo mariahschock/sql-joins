3f176761985630154ca0562712017dbbf2cd5aa9
Mariah <mariahschock@gmail.com>
2022-08-02 15:25:11 -0700

    test 14 passing

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,8 @@
 -- the average film length by category
 -- include the category name and avg length
+
+SELECT name, AVG(length) FROM film
+INNER JOIN film_category ON film_category.film_id = film.film_id
+INNER JOIN category ON category.category_id = film_category.category_id
+GROUP BY category.name
+LIMIT 16;
